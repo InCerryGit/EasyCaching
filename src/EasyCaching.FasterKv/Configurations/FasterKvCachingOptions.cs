c7c88c8822bb2efddfbd1f71d7a68e8a0496e766
InCerry <incerry@foxmail.com>
2022-11-15 21:15:18 +0800

    feat(FasterKv): add FasterKv caching provider support.

---
+++ src/EasyCaching.FasterKv/Configurations/FasterKvCachingOptions.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using EasyCaching.Core.Configurations;
+using FASTER.core;
+using Microsoft.Extensions.Logging;
+
+namespace EasyCaching.FasterKv.Configurations
+{
+    /// <summary>
+    /// FasterKvCachingOptions
+    /// for details, see https://microsoft.github.io/FASTER/docs/fasterkv-basics/#fasterkvsettings
+    /// </summary>
+    public class FasterKvCachingOptions : BaseProviderOptions
+    {
+        /// <summary>
+        /// FasterKv index count
+        /// Each bucket is 64 bits. So this define 65536 keys.
+        /// Used 4MB memory
+        /// </summary>
+        public long IndexCount { get; set; } = 65536;
+
+        /// <summary>
+        /// FasterKv used memory size (default: 16MB)
+        /// </summary>
+        public int MemorySizeBit { get; set; } = 24;
+
+        /// <summary>
+        /// FasterKv page size (default: 1MB) 
+        /// </summary>
+        public int PageSizeBit { get; set; } = 20;
+
+        /// <summary>
+        /// FasterKv read cache used memory size (default: 16MB)
+        /// </summary>
+        public int ReadCacheMemorySizeBit { get; set; } = 24;
+
+        /// <summary>
+        /// FasterKv read cache page size (default: 16MB)
+        /// </summary>
+        public int ReadCachePageSizeBit { get; set; } = 20;
+        
+        /// <summary>
+        /// FasterKv commit logs path
+        /// </summary>
+        public string LogPath { get; set; } = Path.Combine(Environment.CurrentDirectory, $"EasyCaching-FasterKv-{Process.GetCurrentProcess().Id}");
+        
+        /// <summary>
+        /// Set Custom Store
+        /// </summary>
+        public FasterKV<SpanByte, SpanByte>? CustomStore { get; set; }
+
+        internal LogSettings GetLogSettings(string name)
+        {
+            return new LogSettings
+            {
+                LogDevice = Devices.CreateLogDevice(Path.Combine(LogPath, name),
+                    preallocateFile: true,
+                    deleteOnClose: true),
+                PageSizeBits = PageSizeBit,
+                MemorySizeBits = MemorySizeBit,
+                ReadCacheSettings = new ReadCacheSettings
+                {
+                    MemorySizeBits = ReadCacheMemorySizeBit,
+                    PageSizeBits = ReadCachePageSizeBit,
+                }
+            };
+        }
+    }
+}
---
+++ test/EasyCaching.UnitTests/CachingTests/FasterKvCachingProviderTest.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Threading.Tasks;
+using EasyCaching.Core;
+using EasyCaching.Core.Configurations;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EasyCaching.UnitTests.CachingTests;
+
+public class FasterKvCachingProviderTest : BaseCachingProviderTest
+{
+    public FasterKvCachingProviderTest()
+    {
+        _defaultTs = TimeSpan.FromSeconds(30);
+    }
+
+    protected override IEasyCachingProvider CreateCachingProvider(Action<BaseProviderOptions> additionalSetup)
+    {
+        var services = new ServiceCollection();
+        services.AddEasyCaching(x =>
+            x.UseFasterKv(options =>
+                {
+                    options.SerializerName = "msg";
+                    additionalSetup(options);
+                })
+                .WithMessagePack("msg")
+        );
+        IServiceProvider serviceProvider = services.BuildServiceProvider();
+        return serviceProvider.GetService<IEasyCachingProvider>();
+    }
+
+    protected override Task GetAsync_Parallel_Should_Succeed()
+    {
+        return Task.FromResult(1);
+    }
+
+    protected override void Get_Parallel_Should_Succeed()
+    {
+    }
+
+    public override Task GetAllByPrefix_Async_Should_Throw_ArgumentNullException_When_Prefix_IsNullOrWhiteSpace(
+        string preifx)
+    {
+        return Task.CompletedTask;
+    }
+
+    public override void GetAllByPrefix_Should_Throw_ArgumentNullException_When_CacheKey_IsNullOrWhiteSpace(
+        string prefix)
+    {
+    }
+
+    protected override Task Get_Count_Async_With_Prefix_Should_Succeed()
+    {
+        return Task.CompletedTask;
+    }
+
+    protected override Task Get_Count_Async_Without_Prefix_Should_Succeed()
+    {
+        return Task.CompletedTask;
+    }
+
+    protected override void Get_Count_With_Prefix_Should_Succeed()
+    {
+    }
+
+    protected override void Get_Count_Without_Prefix_Should_Succeed()
+    {
+    }
+
+    protected override void GetByPrefix_Should_Succeed()
+    {
+    }
+
+    protected override void GetByPrefix_With_Not_Existed_Prefix_Should_Return_Empty_Dict()
+    {
+    }
+
+    protected override Task GetByPrefixAsync_Should_Succeed()
+    {
+        return Task.CompletedTask;
+    }
+
+    protected override Task GetByPrefixAsync_With_Not_Existed_Prefix_Should_Return_Empty_Dict()
+    {
+        return Task.CompletedTask;
+    }
+
+    protected override Task GetExpiration_Async_Should_Succeed()
+    {
+        return Task.CompletedTask;
+    }
+
+    protected override void GetExpiration_Should_Succeed()
+    {
+    }
+
+    public override void RemoveByPattern_Should_Succeed()
+    {
+    }
+
+    public override Task RemoveByPatternAsync_Should_Succeed()
+    {
+        return Task.CompletedTask;
+    }
+
+    public override Task RemoveByPrefix_Async_Should_Throw_ArgumentNullException_When_Prefix_IsNullOrWhiteSpace(
+        string preifx)
+
+    {
+        return Task.CompletedTask;
+    }
+
+    protected override void RemoveByPrefix_Should_Succeed()
+    {
+    }
+
+    public override void RemoveByPrefix_Should_Throw_ArgumentNullException_When_CacheKey_IsNullOrWhiteSpace(
+        string prefix)
+    {
+    }
+
+    protected override Task RemoveByPrefixAsync_Should_Succeed()
+    {
+        return Task.CompletedTask;
+    }
+}
